e354301f4c1314a717fc8c6d3d3072b71433564d
sadikshyaoli <sadikshyaaoli@gmail.com>
2024-10-05 19:11:02 -0300

    Added UserInterface

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
 import com.wecodeforfood.core.Landsat;
 import com.wecodeforfood.core.TargetLocation;
+import com.wecodeforfood.ui.LandsatSwingApp;
 
 public class Main {
     public static void main(String[] args) {
+        LandsatSwingApp landsatApp = new LandsatSwingApp(); // Create an instance
+
+        if (landsatApp.isSubmitted()){
+            // Get submitted values
+            double latitude = landsatApp.getLatitude();
+            double longitude = landsatApp.getLongitude();
+            int cloudCoverage = landsatApp.getCloudCoverage();
+
+            Landsat landsat = new Landsat(cloudCoverage);
+            TargetLocation targetLocation = new TargetLocation(latitude, longitude);
+
+            // Handle the submitted values
+            System.out.println("Received Values: Latitude: " + latitude + ", Longitude: " + longitude + ", Cloud Coverage: " + cloudCoverage + "%");
+        }
     }
 }
